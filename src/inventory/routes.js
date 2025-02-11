54e3a5eef4b7668b1400206480cc9dc521d7eb10
John Paretz <jmparet@pontpark.edu>
2025-02-11 12:47:27 -0500

    s

--- src/inventory/routes.js
+++ src/inventory/routes.js
@@ -2,10 +2,15 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./controller');
 
-router.get('/potatoes', controller.getAllPotatoes);
+
+
+router.get('/', (req, res) => {  // This is the important part!
+    res.send('Hello from the root path!'); 
+});
+/*router.get('/potatoes', controller.getAllPotatoes);
 router.get('/potatoes/:id', controller.getPotatoById);
 router.post('/potatoes', controller.createPotato);
 router.put('/potatoes/:id', controller.updatePotato);
 router.delete('/potatoes/:id', controller.deletePotato);
-
+*/
 module.exports = router;
