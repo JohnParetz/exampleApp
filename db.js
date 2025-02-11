aa9dc6b254119a3275dad175bec8b09aac83da3f
John Paretz <jmparet@pontpark.edu>
2025-02-11 13:37:34 -0500

    s

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const Pool = require("pg").Pool;
     const pool = new Pool({
         user: "jmparet",
         host: "localhost", 
-        database: "inventory",
+        database: "newapp_u3ga",
         password: "user4",
         port: 5432,
     });
