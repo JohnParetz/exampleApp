67dfba25b5c3407531a5fbf2985784c8d3d1bb5b
John Paretz <jmparet@pontpark.edu>
2025-02-11 12:32:37 -0500

    s

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const routes = require('./src/inventory/routes');
 app.use(express.json()); 
 app.use('/', routes);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
