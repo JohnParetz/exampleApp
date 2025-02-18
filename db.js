5d83d4a7c4dff0e926d482dbcbf01d296f77448f
John Paretz <jmparet@pontpark.edu>
2025-02-18 09:29:05 -0500

    D

--- db.js
+++ db.js
@@ -2,19 +2,15 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: "jmparet", //jmparet
-    host: "dpg-cuh3hgpu0jms73fu27j0-a", //localhost
-    database: "newapp_u3ga", //newapp_u3ga
-    password: "QBI8WWH0EmyToU1ApTdDEgJvdiEUYlxq",  //user4
-    port: 5432, //5432
-
+    user: process.env.DB_jmparet,
+    host: process.env.dpg-DB_dpg-cuh3hgpu0jms73fu27j0-a,
+    database: process.env.DB_newapp_u3ga,
+    password: process.env.DB_QBI8WWH0EmyToU1ApTdDEgJvdiEUYlxq,
+    port: process.env.db.DB_5432,
 });
 
-console.log("Connection details:", pool.options); 
-
-pool.connect() 
+pool.connect()
     .then(() => console.log('Database connected'))
     .catch(err => console.error('Database connection error:', err));
-
 
 module.exports = pool;
