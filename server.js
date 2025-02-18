30736ecf88fc91e2dd86d94ab71750c93b4e2782
John Paretz <jmparet@pontpark.edu>
2025-02-18 08:59:55 -0500

    d

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-const pool = require('../..db');
+const pool = require('./db');
 app.use(express.json());
 
 // Routes
