c21a37903ee17a070f2656431ae7de5576494b11
John Paretz <jmparet@pontpark.edu>
2025-02-18 09:49:09 -0500

    d

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: process.env.DB_jmparet,
-    host: process.env.dpg-DB_dpg-cuh3hgpu0jms73fu27j0-a,
-    database: process.env.DB_newapp_u3ga,
-    password: process.env.DB_QBI8WWH0EmyToU1ApTdDEgJvdiEUYlxq,
-    port: process.env.db.DB_5432,
+    user: process.env.DB_USER || "jmparet",
+    host: process.env.DB_HOST || "dpg-cuh3hgpu0jms73fu27j0-a",
+    database: process.env.DB_NAME || "newapp_u3ga",
+    password: process.env.DB_PASSWORD || "QBI8WWH0EmyToU1ApTdDEgJvdiEUYlxq",
+    port: process.env.DB_PORT || 5432,
 });
 
 pool.connect()
