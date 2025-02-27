080d99b776b297b0609528dddd5052ad6d77a957
John Paretz <jmparet@pontpark.edu>
2025-02-27 18:41:38 -0500

    d

--- db.js
+++ db.js
@@ -1,19 +1,19 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-const pool = new Pool({ // db info, render
-
+const pool = new Pool({
     user: process.env.DB_USER || "jmparet",
     host: process.env.DB_HOST || "dpg-cuh3hgpu0jms73fu27j0-a.ohio-postgres.render.com",
     database: process.env.DB_NAME || "newapp_u3ga",
     password: process.env.DB_PASSWORD || "QBI8WWH0EmyToU1ApTdDEgJvdiEUYlxq",
     port: process.env.DB_PORT || 5432,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
-
-
-pool.connect() 
-    .then(() => console.log('DB connected')) // Log success
-    .catch(err => console.error('DB error:', err)); // Log error *** got this error too could not connect to db***
+pool.connect()
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error('DB error:', err));
 
 module.exports = pool;
