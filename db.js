42591b430e5d7081d3b33a9304ca8c4d3df9dbfa
John Paretz <jmparet@pontpark.edu>
2025-04-07 12:41:06 -0400

    testing

--- db.js
+++ db.js
@@ -3,34 +3,17 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     user: process.env.DB_USER || "jmparet",
-    host: process.env.DB_HOST || "dpg-cuh3hgpu0jms73fu27j0-a",
-    database: process.env.DB_NAME || "newapp_u3ga",
-    password: process.env.DB_PASSWORD || "QBI8WWH0EmyToU1ApTdDEgJvdiEUYlxq",
+    host: process.env.DB_HOST || "dpg-cvcal7hc1ekc73ep60p0-a.ohio-postgres.render.com",
+    database: process.env.DB_NAME || "recipe_qqe5",
+    password: process.env.DB_PASSWORD || "4m2a1h1ECI1pUooD1mah8XFYrnQvCVMI",
     port: process.env.DB_PORT || 5432,
     ssl: {
-        rejectUnauthorized: false, 
-        required: true,
+        rejectUnauthorized: false,
     },
 });
 
-const connectWithTimeout = async () => {
-    const timeout = 5000;
-    const timeoutPromise = new Promise((_, reject) =>
-        setTimeout(() => reject(new Error('Database connection timeout')), timeout)
-    );
-
-    try {
-        await Promise.race([pool.connect(), timeoutPromise]);
-        console.log('DB connected successfully');
-
-        const result = await pool.query('SELECT 1');
-        console.log('Database test query successful:', result.rows);
-    } catch (err) {
-        console.error('DB connection error:', err);
-        console.error('DB connection error details:', err.message, err.stack);
-    }
-};
-
-connectWithTimeout();
+pool.connect()
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error('DB error:', err));
 
 module.exports = pool;
--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 require('dotenv').config();
 const express = require('express');
-const helmet = require('helmet');
 const app = express();
+const port = process.env.PORT || 5000;
 const pool = require('./db');
 const path = require('path');
-const cors = require('cors');
 
-app.use(cors());
 app.use(express.json());
-app.use(helmet());
 app.use(express.static(path.join(__dirname, '')));
 
 app.get('/api/recipes', async (req, res) => {
@@ -98,8 +95,6 @@ app.get('/api/search', async (req, res) => {
     }
 });
 
-const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
-    console.log(`HTTP server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
